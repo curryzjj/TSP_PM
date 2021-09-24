fb6b3262c76235ea71c1eaaec2b998cf33ed9d09
curryzjj <curry_zhao@hust.edu.cn>
2021-09-24 11:03:49 +0800

    Update PMManager LLPL Test

--- Common/src/main/java/System/Constants.java
+++ Common/src/main/java/System/Constants.java
@@ -9,5 +9,5 @@ public interface Constants {
     char virtualType = 'v';
     //PM constants
     String Default_Heap_Path="/mnt/pmem0/jjzhao";
-    String Default_Heap_Name="default";
+    String Default_Heap_Name="/default";
 }
