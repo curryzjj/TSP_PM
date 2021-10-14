52b067861ea932ad58f44d51b23d4c557129217b
curryzjj <curry_zhao@hust.edu.cn>
2021-10-14 08:58:50 +0800

    Update

---
+++ TransactionalProcess/src/main/java/engine/Clock.java
@@ -0,0 +1,11 @@
+package engine;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+public class Clock implements Closeable {
+    @Override
+    public void close() throws IOException {
+
+    }
+}
