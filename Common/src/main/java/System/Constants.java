e042b6bf5f07bc5cbe159774705ef3a507340863
curryzjj <curry_zhao@hust.edu.cn>
2022-06-27 16:17:46 +0800

    Add TxnResult

--- Common/src/main/java/System/Constants.java
+++ Common/src/main/java/System/Constants.java
@@ -5,7 +5,7 @@ public interface Constants {
     String Mac_Project_Path = System.getProperty("user.home").concat("/hair-loss/TSP_PM");
     String Node22_Project_Path = "/home/jjzhao/TSP_PM";
     String Mac_Data_Path = System.getProperty("user.home").concat("/hair-loss/app/benchmarks/");
-    String SSD_Path = "/mnt/nvme0n1p1/jjzhao";
+    String SSD_Path = "/mnt/nvme0n1p2/jjzhao";
     String Node22_Data_Path = SSD_Path.concat("/app/benchmarks/");
     //PM constants
     String Default_Heap_Path = "/mnt/persist-memory/pmem0/jjzhao";
